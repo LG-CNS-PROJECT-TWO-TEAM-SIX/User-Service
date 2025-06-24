d3febe60ea06f95e456f0983af931aca79e350e9
하리보냠 <drppr99@gmail.com>
2025-06-24 12:31:01 +0900

    [FXI] DTO

--- src/main/java/com/example/User_Service/api/dto/request/UserLoginRequest.java
+++ src/main/java/com/example/User_Service/api/dto/request/UserLoginRequest.java
@@ -1,8 +1,12 @@
 package com.example.User_Service.api.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class UserLoginRequest {
     private String email;
     private String password;
--- src/main/java/com/example/User_Service/api/dto/request/UserSignUpRequest.java
+++ src/main/java/com/example/User_Service/api/dto/request/UserSignUpRequest.java
@@ -2,10 +2,14 @@ package com.example.User_Service.api.dto.request;
 
 import com.example.User_Service.domain.model.Role;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class UserSignUpRequest {
     private String email;
     private String password;
